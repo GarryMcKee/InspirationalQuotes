82f405c7338450e963a63139dfe0c20ca3198030
Garry McKee <gmckee@logitech.com>
2016-12-23 17:39:10 +0000

    Add Log and LOG_TAG

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,15 @@ package com.mellobit.gmckee.inspirationalquotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String LOG_TAG = this.getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "A log!");
     }
 }
